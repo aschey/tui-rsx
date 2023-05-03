a65ac7cdcbd78c8480b526d3fd4942588fa52b13
Austin Schey <aschey13@gmail.com>
2023-05-03 01:10:13 -0500

    add paragraph widget

--- crates/tui-rsx/examples/example.rs
+++ crates/tui-rsx/examples/example.rs
@@ -1,17 +1,43 @@
-use ratatui::{backend::TestBackend, Terminal};
+use std::{error::Error, io};
+
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use ratatui::{
+    backend::CrosstermBackend,
+    style::{Color, Style},
+    Terminal, TerminalOptions, Viewport,
+};
 use tui_rsx::prelude::*;
 
-pub fn main() {
-    let backend = TestBackend::new(32, 32);
-    let mut terminal = Terminal::new(backend).unwrap();
+pub fn main() -> Result<(), Box<dyn Error>> {
+    enable_raw_mode()?;
+
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::with_options(
+        backend,
+        TerminalOptions {
+            viewport: Viewport::Inline(8),
+        },
+    )?;
+
     terminal
         .draw(|f| {
             let view = rsx! {
                 <Column>
-                    <block title="test"/>
+                    <paragraph
+                        length=2
+                        text="yo"
+                        style=Style::default().fg(Color::Green)
+                        block = Block::default().title("title2")
+                    />
+                    <block title="title"/>
                 </Column>
             };
             view(f, f.size());
         })
         .unwrap();
+
+    disable_raw_mode()?;
+    println!();
+    Ok(())
 }
--- crates/tui-rsx/src/lib.rs
+++ crates/tui-rsx/src/lib.rs
@@ -5,10 +5,39 @@ pub mod prelude {
 }
 
 use prelude::*;
-use ratatui::{backend::Backend, layout::Rect, Frame};
+use ratatui::{backend::Backend, layout::Rect, text::Text, Frame};
+
+pub trait CustomDefault {
+    fn default() -> Self;
+}
 
 pub type BlockProps<'a> = Block<'a>;
 
 pub fn block<B: Backend>(frame: &mut Frame<B>, rect: Rect, props: BlockProps) {
+    frame.render_widget(props, rect);
+}
+
+pub type ParagraphProps<'a> = Paragraph<'a>;
+
+pub trait ParagraphPropsExt<'a>
+where
+    Self: 'a,
+{
+    fn text(self, text: impl Into<Text<'a>>) -> Self;
+}
+
+impl<'a> ParagraphPropsExt<'a> for Paragraph<'a> {
+    fn text(self, text: impl Into<Text<'a>>) -> Self {
+        Self::new(text)
+    }
+}
+
+impl<'a> CustomDefault for Paragraph<'a> {
+    fn default() -> Self {
+        Self::new(Text::default())
+    }
+}
+
+pub fn paragraph<B: Backend>(frame: &mut Frame<B>, rect: Rect, props: ParagraphProps) {
     frame.render_widget(props, rect);
 }
